fcccea8f55886a63cb2fc33f9b1913c9c93a478f
kripul <kripul@users.noreply.github.com>
2025-02-03 11:52:35 +0700

    extend

--- windows-resize.bat
+++ windows-resize.bat
@@ -1,9 +1,9 @@
 @echo off
-mode con cp select=437 >nul
+@REM mode con cp select=437 >nul
 
-set C=%SystemDrive:~0,1%
-for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<installer\>"') do (echo select vol %%a & echo delete partition) | diskpart
-for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<%C%\>"') do (echo select vol %%a & echo extend) | diskpart
+@REM set C=%SystemDrive:~0,1%
+@REM for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<installer\>"') do (echo select vol %%a & echo delete partition) | diskpart
+@REM for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<%C%\>"') do (echo select vol %%a & echo extend) | diskpart
 
 ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\diskpart.extend"
 ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
