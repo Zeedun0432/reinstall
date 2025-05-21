f9884190f19b9ce21e76b35a7f366608d65b79af
Zeedun0432 <zidanlagi864@gmail.com>
2025-05-22 06:00:51 +0700

    reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,8 +3,8 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/kripul/reinstall/main
-confhome_cn=https://jihulab.com/kripul/reinstall/-/raw/main
+confhome=https://raw.githubusercontent.com/Zeedun0432/reinstall/main
+confhome_cn=https://jihulab.com/Zeedun0432/reinstall/-/raw/main
 # confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 # 默认密码
